b944c1dece958db308db9a9311958090baec106e
Farhan Tahmid <farhantahmid881@gmail.com>
2023-06-07 21:43:34 +0600

    file and process done

--- main .cpp
+++ main .cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <sstream>
 #include <vector>
 #include <cstring>
@@ -48,7 +49,69 @@ std::vector<std::string> parseArguments(const std::string& command) {
     return args;
 }
 
-int main() {
+void executeFileManagement(){
+    std::cout << "Enter a command ('exit' to quit): " << std::endl;
+    std::string command;
+    std::string filename;
+    std::string content;
+
+    while (true) {
+        std::getline(std::cin, command);
+
+        if(command.substr(0,4)=="exit"){
+            break;
+        }
+        
+        if (command.substr(0, 4) == "cat ") {
+            filename = command.substr(4);
+            std::ifstream file(filename);
+            std::string line;
+
+            if (file.is_open()) {
+                while (getline(file, line)) {
+                    std::cout << line << std::endl;
+                }
+                file.close();
+            }
+            else {
+                std::cout << "File does not exist" << std::endl;
+            }
+        }
+        else if (command.substr(0, 6) == "gedit ") {
+            filename = command.substr(6);
+            std::string notepadCommand = "\"C:\\Program Files\\Notepad++\\notepad++.exe\" " + filename;//while doing in linux terminal replace the path with nano.
+            std::system(notepadCommand.c_str());
+        }
+
+
+        else if (command.substr(0, 6) == "echo \"") {
+            std::size_t pos = command.find("\" > ");
+            if (pos != std::string::npos) {
+                content = command.substr(6, pos - 6);
+                filename = command.substr(pos + 4);
+                std::ofstream file(filename, std::ios::app); 
+                file << content << std::endl;
+                file.close();
+            }
+            else {
+                std::cout << "Invalid command format" << std::endl;
+            }
+        }// opens a file
+        else if(command.substr(0,6)=="touch "){
+            filename=command.substr(6);
+             std::ofstream file(filename);
+            if(file){
+                continue;
+            }
+        }
+        else {
+            std::cout << "Invalid command" << std::endl;
+        }
+    }
+}
+
+void executeProcessManagement(){
+
     while (true) {
         std::string command;
         std::cout << "Enter a command ('exit' to quit): ";
@@ -66,6 +129,28 @@ int main() {
 
         executeCommand(args);
     }
+    
+}
 
+int main() {
+    
+    while(true){
+        std::string command;
+        std::cout<<"Enter what system calls you want to execute:\n1)File Management\n2)Process Management\nTo kill the program, press 0.\n";
+        std::getline(std::cin, command);
+
+
+        if(command=="0"){
+            break;
+        }else{
+            if(command=="1"){
+                executeFileManagement();
+            }else if(command=="2"){
+                executeProcessManagement();
+            }
+        }
+
+
+    }
     return 0;
 }
