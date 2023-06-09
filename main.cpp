bde55c4fb785f9ffd7d19ca0701a936ece1c6b19
Farhan Tahmid <farhantahmid881@gmail.com>
2023-06-09 23:44:42 +0600

    project complete

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 const int MAX_ARGS = 10;
 const int MAX_BUFFER_SIZE = 256;
@@ -79,7 +81,7 @@ void executeFileManagement(){
         }
         else if (command.substr(0, 6) == "gedit ") {
             filename = command.substr(6);
-            std::string notepadCommand = "\"C:\\Program Files\\Notepad++\\notepad++.exe\" " + filename;//while doing in linux terminal replace the path with nano.
+            std::string notepadCommand = "nano " + filename;//while doing in linux terminal replace the path with nano.
             std::system(notepadCommand.c_str());
         }
 
@@ -131,12 +133,94 @@ void executeProcessManagement(){
     }
     
 }
+// Function to execute a command and capture the output
+std::string executeInfoCommand(const std::string& command){
+        std::string output;
+        FILE* pipe = popen(command.c_str(), "r");
+        if (pipe) {
+            char buffer[128];
+            while (!feof(pipe)) {
+                if (fgets(buffer, 128, pipe) != nullptr)
+                    output += buffer;
+            }
+            pclose(pipe);
+        }
+        return output;
+}
+
+void sleep(){
+    std::cout << "Before sleep" << std::endl;
+
+    struct timespec sleepTime;
+    sleepTime.tv_sec = 5; // Sleep for 5 seconds
+    sleepTime.tv_nsec = 0;
+
+    int result = nanosleep(&sleepTime, nullptr);
+    if (result != 0) {
+        std::cerr << "Failed to sleep." << std::endl;
+    }
+
+    std::cout << "After sleep" << std::endl;
+}
+
+void executeInformationManagement(){
+    std::string command;
+    
+    while (true) {
+        std::cout << "Enter a command ('exit' to quit): ";
+        std::getline(std::cin, command);
+
+        if (command == "exit")
+            break;
+
+        if (command == "pidof") {
+            std::string programName;
+            std::cout << "Enter the program name: ";
+            std::getline(std::cin, programName);
+            std::string result = executeInfoCommand("pidof " + programName);
+            std::cout << "pidof: " << result << std::endl;
+        } else if (command == "pgrep") {
+            std::string programName;
+            std::cout << "Enter the program name: ";
+            std::getline(std::cin, programName);
+            std::string result = executeInfoCommand("pgrep " + programName);
+            std::cout << "pgrep: " << result << std::endl;
+        } else if (command == "ps") {
+            std::string result = executeInfoCommand("ps");
+            std::cout << "ps: " << result << std::endl;
+        } else if (command == "pstree") {
+            std::string result = executeInfoCommand("pstree");
+            std::cout << "pstree: " << result << std::endl;
+        } else if (command == "ss") {
+            std::string result = executeInfoCommand("ss");
+            std::cout << "ss: " << result << std::endl;
+        } else if (command == "netstat") {
+            std::string result = executeInfoCommand("netstat");
+            std::cout << "netstat: " << result << std::endl;
+        } else if (command == "lsof") {
+            std::string result = executeInfoCommand("lsof");
+            std::cout << "lsof: " << result << std::endl;
+        } else if (command == "fuser") {
+            std::string fileName;
+            std::cout << "Enter the file name: ";
+            std::getline(std::cin, fileName);
+            std::string result = executeInfoCommand("fuser " + fileName);
+            std::cout << "fuser: " << result << std::endl;
+        } else if (command == "sleep") {
+            sleep();
+        }
+        else {
+            std::cout << "Invalid command!" << std::endl;
+        }
+    }
+}
+
 
 int main() {
     
     while(true){
         std::string command;
-        std::cout<<"Enter what system calls you want to execute:\n1)File Management\n2)Process Management\nTo kill the program, press 0.\n";
+        std::cout<<"Enter what system calls you want to execute:\n1)File Management\n2)Process Management\n3)Information Management\nTo kill the program, press 0.\n";
         std::getline(std::cin, command);
 
 
@@ -147,6 +231,8 @@ int main() {
                 executeFileManagement();
             }else if(command=="2"){
                 executeProcessManagement();
+            }else if(command=="3"){
+                executeInformationManagement();
             }
         }
 
